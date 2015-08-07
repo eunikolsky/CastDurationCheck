98da3e981c1b74d99bea80c00740f65fcdddf0a8
Eugene Nikolskyi <eugene.nikolskyi@lge.com>
2015-08-07 14:14:54 -0700

    Initial Commit

---
+++ CastDurationCheck/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CastDurationCheck
+//
+//  Created by Eugene Nikolskyi on 2015-08-07.
+//  Copyright (c) 2015 LG Electronics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CastDurationCheck/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CastDurationCheck
+//
+//  Created by Eugene Nikolskyi on 2015-08-07.
+//  Copyright (c) 2015 LG Electronics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
